960526309bf9052189fb9cc190e9f6d327dbbd69
Blessing <bigbadumhe@gmail.com>
2022-04-03 02:07:39 -0500

    search button made

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let citiesbox = document.querySelector('.citiesbox')
+
+let button = document.querySelector('.btn')
+
+
+const saveToList = () => {
+    
+}
+
+
+
+
+button.addEventListener('click', saveToList)
